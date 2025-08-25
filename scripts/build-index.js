db8d12e0c532aa92db4670e3e8fce26a6512dac5
Louis Ouellet <louis_ouellet@hotmail.com>
2025-08-25 11:11:13 -0400

    General: Initial Commit

---
+++ scripts/build-index.js
@@ -0,0 +1,17 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+function buildIndex(dir) {
+  const abs = path.resolve(dir);
+  const files = fs.readdirSync(abs)
+    .filter(f => f.startsWith('_') && f.endsWith('.scss') && f !== '_index.scss')
+    .map(f => `@forward "${path.basename(f, '.scss')}";`)
+    .join('\n');
+
+  fs.writeFileSync(path.join(abs, '_index.scss'), files + '\n');
+}
+
+buildIndex('src/scss/components');
+buildIndex('src/scss/utilities');
+buildIndex('src/scss/forms');
+console.log('Indexes rebuilt!');
